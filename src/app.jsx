20bf9e5f2ab7b4347649afcfb91f755550569bc5
Dzmitry Hrachou <dhrachou@slb.com>
2018-04-13 18:29:13 +0300

    4 types of rendering

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
+// 1
+// Stateless render
+const ReactStateless = () => {
     return (
         <div>
             <p>Hello World: stateless component!</p>
         </div>
     );
 };
-export default App;
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(<ReactStateless/>, document.getElementById("app1"));
+
+//2
+//create element
+const elem1 = React.createElement("p", {className: "p-class"}, "Hello world: create element");
+const root = React.createElement("div", null, elem1);
+ReactDOM.render(root, document.getElementById("app2"));
+
+// 3
+//ReactComponent
+class ReactComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Hello World: react component!</p>
+            </div>
+        );
+    }
+}
+ReactDOM.render(<ReactComponent/>, document.getElementById("app3"));
+
+// 4
+//pure render
+ReactDOM.render((<p>Hello World: pure html</p>), document.getElementById("app4"));
