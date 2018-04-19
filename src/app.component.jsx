fa9f8c0551b569989930bb7812bdef868ea69418
Dzmitry Hrachou <dhrachou@slb.com>
2018-04-19 16:16:01 +0300

    react-router-dom has been refactored

--- src/app.component.jsx
+++ src/app.component.jsx
@@ -1,6 +1,5 @@
 import React, {PureComponent} from "react";
-import {BrowserRouter, Route} from "react-router-dom";
-import ReactDOM from "react-dom";
+import {Route, Link, Switch} from "react-router-dom";
 
 import MovieListComponent from "component/movie-list/movie-list.component";
 import MovieInfoComponent from "component/movie-info/movie-info.component";
@@ -9,33 +8,37 @@ import MovieSearchComponent from "component/movie-search/movie-search.component"
 import "app.style";
 
 class AppComponent extends PureComponent {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
-            <BrowserRouter>
-                <div className="app-layout">
-                    <header>
-                        <Route path="/" component={MovieSearchComponent}/>
+            <div className="app-layout">
+                <header>
+                    <Switch>
+                        <Route exact path="/" component={MovieSearchComponent}/>
                         <Route path="/movie-list" component={MovieSearchComponent}/>
                         <Route path="/movie-list/:movieId" component={MovieInfoComponent}/>
-                    </header>
-                    <main>
-                        <Route path="/" component={MovieListComponent}/>
+                    </Switch>
+                </header>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/movie-list">movie-list</Link></li>
+                        <li><Link to="/movie-list/12">movie-info 12</Link></li>
+                    </ul>
+                </nav>
+                <main>
+                    <Switch>
+                        <Route exact path="/" component={MovieListComponent}/>
                         <Route path="/movie-list" component={MovieListComponent}/>
-                    </main>
-                    <footer>
-                        <div>
-                            <p>Netflix</p>
-                        </div>
-                    </footer>
-                </div>
-            </BrowserRouter>
+                    </Switch>
+                </main>
+                <footer>
+                    <div>
+                        <p>Netflix</p>
+                    </div>
+                </footer>
+            </div>
         );
     }
 }
 
 export default AppComponent;
-ReactDOM.render(<AppComponent/>, document.body);
--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,10 @@
-import "./app.component";
+import React from "react";
+import {BrowserRouter} from "react-router-dom";
+import ReactDOM from "react-dom";
+import AppComponent from "./app.component";
+
+ReactDOM.render(
+    <BrowserRouter>
+        <AppComponent/>
+    </BrowserRouter>
+    , document.getElementById("app"));
